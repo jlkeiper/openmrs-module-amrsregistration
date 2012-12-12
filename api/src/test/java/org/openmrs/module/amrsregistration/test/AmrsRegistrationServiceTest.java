ebcbb98248591eb0259d68ca71cf6be08c64570a
Jeremy Keiper <jeremy@openmrs.org>
2012-12-11 23:58:17 -0500

    removed pointless test

--- api/src/test/java/org/openmrs/module/amrsregistration/test/AmrsRegistrationServiceTest.java
+++ api/src/test/java/org/openmrs/module/amrsregistration/test/AmrsRegistrationServiceTest.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.amrsregistration.test;
 
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -8,9 +7,7 @@ import org.junit.Test;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.Person;
-import org.openmrs.PersonName;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.amrsregistration.AmrsRegistrationService;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 public class AmrsRegistrationServiceTest extends BaseModuleContextSensitiveTest {
@@ -29,34 +26,5 @@ public class AmrsRegistrationServiceTest extends BaseModuleContextSensitiveTest 
 			}
 			Assert.assertNotNull(localPatient.getIdentifiers());
 		}
-	}
-
-	@Test
-	@Ignore("not sure what this is supposed to do")
-	public void shouldGetPersonsUsingAnyPersonProperties() {
-		Person localPerson1 = Context.getPersonService().getPerson(
-				Integer.valueOf(364));
-		AmrsRegistrationService localAmrsRegistrationService = (AmrsRegistrationService) Context
-				.getService(AmrsRegistrationService.class);
-		PersonName localPersonName = new PersonName();
-		localPersonName.setGivenName("luc");
-		List<Person> localList = localAmrsRegistrationService.getPersons(null,
-				null, null, null, null, localPerson1.getAge(), -1);
-
-		Assert.assertTrue(localList.size() > 0);
-		Assert.assertNotNull(localList.get(0));
-		System.out.println("apiPerson: " + localPerson1.getPersonId());
-		System.out.println("modulePerson " + localPerson1.getPersonId());
-
-		Assert.assertNotNull(localAmrsRegistrationService);
-		for (Person localPerson2 : localList) {
-			Patient localPatient = (Patient) localPerson2;
-			System.out.println("id: " + localPatient.getPersonId() + ", name: "
-					+ localPatient.getPersonName().getFamilyName() + ", "
-					+ localPatient.getPersonName().getGivenName() + ", "
-					+ localPatient.getPatientIdentifier());
-		}
-
-		System.out.println("results: " + localList.size());
 	}
 }
